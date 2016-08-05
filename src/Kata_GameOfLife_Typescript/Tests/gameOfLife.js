446cd5ba582f96e3698c1f7f6dbecfda859adbe7
Ntokozo Shagala <ntokozo_studio@outlook.com>
2016-08-05 17:01:53 +0200

    Initialized first unit test to check if jasmine-karma is running well

---
+++ src/Kata_GameOfLife_Typescript/Tests/gameOfLife.js
@@ -0,0 +1,7 @@
+/// <reference path="../typings/globals/jasmine/index.d.ts" />
+describe("test", function () {
+    it("should check if will run this test", function () {
+        console.log("Test run!");
+    });
+});
+//# sourceMappingURL=gameOfLife.js.map
---
+++ src/Kata_GameOfLife_Typescript/Tests/gameOfLife.spec.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../typings/globals/jasmine/index.d.ts" />
+
+describe("test", () => {
+    it("should check if will run this test", () => {
+        console.log("Test run by Ntokozo hooreh!");
+    });
+});
